4dd91affdddc8769b276986e45c0a2b56c7a743a
Dayne Dewar <daynedewar@yahoo.com>
2021-04-08 18:52:08 -0400

    just pushing

--- app/controllers/user_events_controller.rb
+++ app/controllers/user_events_controller.rb
@@ -1,2 +1,18 @@
 class UserEventsController < ApplicationController
+    def index
+        user_events = UserEvent.order(:id)
+        render json: user_events
+    end
+
+    def create
+        user_event = UserEvent.create(user_events_params)
+        render json: user_event
+    end
+
+    private 
+
+    def user_events_params
+        params.permit(:user_id, :event_id)
+    end
+
 end
